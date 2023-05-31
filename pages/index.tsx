9cc33193e4ebc8d970aae23b3503c6b0ec97f1a3
Muhammad Bilal <2019cs682@student.uet.edu.pk>
2023-05-31 12:25:50 +0500

    work on home blog component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import HomeBanner from "@/app/pageComponents/Home/HomeBanner";
 import HomeServicesBlock from "@/app/pageComponents/Home/HomeServicesBlock";
 import HomeProjectsBlock from "@/app/pageComponents/Home/HomeProjectsBlock";
 import HomeBlogsBlock from "@/app/pageComponents/Home/HomeBlogsBlock";
+import Footer from "@/app/Components/Footer";
 
 const Home = () => {
   return (
@@ -18,9 +19,9 @@ const Home = () => {
         <HomeBlogsBlock />
       </section>
 
-      <Box className={styles.footer}>
+      <div>
         <Footer />
-      </Box>
+      </div>
     </div>
   );
 };
