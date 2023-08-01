ebbd4c7439197f851f2ffccbd9a8880303444049
Muhammad Bilal <2019cs682@student.uet.edu.pk>
2023-08-01 19:47:50 +0500

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-  plugins: [
-    "postcss-preset-env",
-    {
-      // Specify the stage of features, or set it to 'autoprefixer' if you only want autoprefixer.
-      stage: 1,
-      features: {
-        "nesting-rules": true, // Enable nesting for CSS Modules
-      },
-    },
-    "tailwindcss",
-    "autoprefixer",
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
