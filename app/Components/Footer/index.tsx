2d9443644192b65b6dc61ce8ced44e4617376c8d
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2023-08-26 21:31:27 +0500

    fixed footer

--- app/Components/Footer/index.tsx
+++ app/Components/Footer/index.tsx
@@ -2,10 +2,15 @@ import React, { FC, ReactElement } from "react";
 import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 
+import FacebookSharpIcon from "@mui/icons-material/FacebookSharp";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
 export const Footer: FC = (): ReactElement => {
   return (
-    <div className="w-full h-[500px] bg-[#E7EBFB] pt-8 px-[4.8rem]">
-      <h1 className="text-center font-lato text-[26px]">
+    <div className="w-full min-h-[500px] bg-[#E7EBFB] pt-8 px-[1.5rem] sm:px-[4.8rem]">
+      <h1 className="text-center font-lato text-2xl sm:text-[26px]">
         Want to start your projects with us?
       </h1>
       <p className="text-center mt-2">
@@ -31,7 +36,7 @@ export const Footer: FC = (): ReactElement => {
             title="logo"
             className="block"
           />
-          <h2 className="text-black font-poppins font-semibold text-2xl tracking-wide mt-7">
+          <h2 className="text-black font-serif italic font-normal text-lg tracking-wide mt-7">
             Head Office
           </h2>
           <p className="text-black font-poppins text-[14px] font-normal tracking-wider">
@@ -47,13 +52,7 @@ export const Footer: FC = (): ReactElement => {
           <h2 className="text-black font-poppins text-lg font-semibold tracking-wide">
             Company
           </h2>
-          {[
-            "About Us",
-            "Services",
-            "Projects",
-            "Contact Us",
-            "Privacy Policy",
-          ].map((v, i) => {
+          {["About Us", "Services", "Projects"].map((v, i) => {
             return (
               <p
                 key={i}
@@ -68,13 +67,7 @@ export const Footer: FC = (): ReactElement => {
           <h2 className="text-black font-poppins text-lg font-semibold tracking-wide">
             Support
           </h2>
-          {[
-            "About Us",
-            "Services",
-            "Projects",
-            "Contact Us",
-            "Privacy Policy",
-          ].map((v, i) => {
+          {["Contact Us", "Privacy Policy"].map((v, i) => {
             return (
               <p
                 key={i}
@@ -86,26 +79,65 @@ export const Footer: FC = (): ReactElement => {
           })}
         </div>
         <div className="border-2 border-solid border-grey-500 m-0">
-          <h2 className="text-black font-poppins text-lg font-semibold tracking-wide">
+          <h2 className="text-black font-poppins text-lg font-semibold tracking-wide mt-2">
             Follow us
           </h2>
-          {[
-            "About Us",
-            "Services",
-            "Projects",
-            "Contact Us",
-            "Privacy Policy",
-          ].map((v, i) => {
-            return (
-              <p
-                key={i}
-                className="text-black font-poppins text-[14px] font-normal tracking-wider mt-3 hover:underline cursor-pointer"
-              >
-                {v}
-              </p>
-            );
-          })}
+          <div className="flex flex-row">
+            {[
+              {
+                icon: (
+                  <FacebookSharpIcon
+                    sx={{ color: "#4267B2", fontSize: "35px" }}
+                  />
+                ),
+              },
+              {
+                icon: (
+                  <LinkedInIcon sx={{ color: "#0072b1", fontSize: "35px" }} />
+                ),
+              },
+              {
+                icon: (
+                  <YouTubeIcon sx={{ color: "#CD201F", fontSize: "35px" }} />
+                ),
+              },
+              {
+                icon: (
+                  <TwitterIcon sx={{ color: "#00acee", fontSize: "35px" }} />
+                ),
+              },
+            ].map((v, i) => {
+              return (
+                <p
+                  key={i}
+                  className="text-black font-poppins text-[14px] font-normal tracking-wider hover:underline cursor-pointer mr-3 mt-0"
+                >
+                  {v.icon}
+                </p>
+              );
+            })}
+          </div>
         </div>
+      </div>
+      <div className="flex justify-between mt-2 pb-2">
+        <p className="text-black font-poppins text-[14px] font-normal tracking-wider">
+          © 2021 Alumtec Glazing Solutions Inc. All rights reserved.
+        </p>
+        <p>
+          <span className="text-black font-poppins text-[14px] font-normal tracking-wider">
+            Powered by
+          </span>
+          <span className="text-black font-poppins text-[14px] font-normal tracking-wider">
+            {" "}
+            <a
+              href="https://www.linkedin.com/in/muhammad-bilal-028843199/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Muhammad Bilal Mohib
+            </a>
+          </span>
+        </p>
       </div>
     </div>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,25 +6,6 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>AlumTec</title>
-      {/* Font Awesome */}
-      {/* <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-        rel="stylesheet"
-      /> */}
-      {/* Google Fonts */}
-      {/* <link
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        rel="stylesheet"
-      /> */}
-      {/* MDB */}
-      {/* <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.css"
-        rel="stylesheet"
-      /> */}
-      <meta
-        name="description"
-        content="Aluminium curtain wall, windows and doors are our speciality."
-      />
       {/* <!-- ALUM TEC ICON --> */}
       <link rel="icon" href="/logo.jxr" type="image/x-icon"></link>
       <body>
