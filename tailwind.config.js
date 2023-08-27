576d8a560efe3f5e18da3cbc15d5f6935d337451
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2023-08-27 06:22:37 +0500

    updated tailwind css file
    - To fix production fonts application fail
    - To make the production build use the fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+  important: true,
+  purge: [
+    "./app/Components/**/*.{js,ts,jsx,tsx}",
+    "./app/pageComponents/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "/styles/**/*.{css,module.css,scss,sass}",
+  ],
+  content: [
+    "./app/Components/**/*.{js,ts,jsx,tsx}",
+    "./app/pageComponents/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "/styles/**/*.{css,module.css,scss,sass}",
   ],
   theme: {
     fontFamily: {
