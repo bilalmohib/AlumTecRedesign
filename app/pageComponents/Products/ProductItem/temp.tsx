fe8d8ba6eaeeb2961730c68609e01a5b8f5e9490
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2023-08-28 12:51:36 +0500

    working on products responsiveness

--- app/pageComponents/Products/ProductItem/temp.tsx
+++ app/pageComponents/Products/ProductItem/temp.tsx
@@ -65,14 +65,16 @@ const ProductItem = ({
         // border: "5px solid red",
       }}
       className={`
-      bg-cover bg-repeat w-full h-[542px] px-10
-          flex ${
-            direction === "left" ? "flex-row" : "flex-row-reverse"
-          } justify-between
+      bg-cover bg-repeat lg:w-full lg:h-[542px] px-14 lg:px-20
+          flex flex-col h-full ${
+            direction === "left"
+              ? "flex-row lg:flex-row"
+              : "flex-row-reverse lg:flex-row-reverse"
+          } lg:justify-between justify-center items-center
       `}
     >
       <div
-        className={`w-6/12 h-full flex flex-row justify-center items-center
+        className={`w-full lg-w-6/12 h-full flex flex-row justify-center items-center
       ${
         direction === "left"
           ? "mt-[-5px] lg:mt-[-30px] xl:mt-[-50px] ml-0"
@@ -89,29 +91,33 @@ const ProductItem = ({
           >
             {title}
           </motion.h1>
-          <ul className="mt-4 mb-4 text-2xl">
-            {descriptionList.map((item: productItemProps, index: number) => {
-              return (
-                <motion.li
-                  className="ml-[-5px] mb-2"
-                  key={index}
-                  variants={staggeredListVariants}
-                  custom={index}
-                  initial="hidden"
-                  animate="visible"
-                >
-                  {item.title}
-                </motion.li>
-              );
-            })}
-          </ul>
+          <div className="flex flex-col justify-center items-center lg:block">
+            <ul className="mt-4 mb-4 text-2xl">
+              {descriptionList.map((item: productItemProps, index: number) => {
+                return (
+                  <motion.li
+                    className=" lg:ml-[-5px] mb-2"
+                    key={index}
+                    variants={staggeredListVariants}
+                    custom={index}
+                    initial="hidden"
+                    animate="visible"
+                  >
+                    {item.title}
+                  </motion.li>
+                );
+              })}
+            </ul>
+          </div>
           <br />
-          <HTBLeftSliderControls />
+          <div className="flex justify-center items-center lg:block">
+            <HTBLeftSliderControls />
+          </div>
         </div>
       </div>
       <motion.div
-        className={`w-6/12 flex flex-row justify-center
-        ${direction === "left" ? "mr-[20px]" : "ml-[-25px]"}
+        className={`w-full lg:w-6/12 flex flex-row justify-center
+        ${direction === "left" ? "lg:mr-[20px]" : "lg:ml-[-25px]"}
         `}
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
