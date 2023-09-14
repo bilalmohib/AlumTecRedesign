ee67fbcafa714b7538c84baf6f2930d8b389a0f3
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2023-09-15 00:51:35 +0500

    fixed

--- app/pageComponents/Blog/BlogBanner/index.tsx
+++ app/pageComponents/Blog/BlogBanner/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 const BlogBanner = () => {
   return (
     <div className="bg-[url('/Images/Blogs/banner.png')] h-60 sm:h-[80vh] mt-[105px] w-full bg-cover bg-no-repeat bg-center">
@@ -7,17 +9,15 @@ const BlogBanner = () => {
         Page under construction. Stay tuned! 🚧
       </p> */}
       <div className="relative h-60 sm:h-[80vh] w-full backdrop-filter backdrop-blur-0 ease-linear transition">
-              <div className="absolute inset-0 bg-black opacity-10"></div>
+        <div className="absolute inset-0 bg-black opacity-10"></div>
         <div className="px-7 sm:px-8 md:px-6 absolute top-0 left-0 right-0 bottom-0 mt-8 sm:mt-[15vh]">
           <div className="w-full flex flex-col justify-center items-center">
-            {/* <p className="text-left text-lg text-white mx-auto font-sans tracking-widest font-extralight">
-              Careers at Alumtec
-            </p> */}
-            <h1 className="mt-0 text-2xl sm:text-6xl font-normal font-sharp sm:font-lato text-white text-left">
-                          <h3 className="text-blue-800 m-0">AlumTec</h3> 
-                          brings the latest <span className="text-blue-800">Alumium</span> 
-                          <h3 className="text-teal-600 m-0">Insights</h3>  to your knowledge
-            </h1>
+            <div className="mt-0 text-2xl sm:text-6xl font-normal font-sharp sm:font-lato text-white text-left">
+              <h3 className="text-blue-800 m-0">AlumTec</h3>
+              brings the latest{" "}
+              <p className="text-blue-800 inline m-0">Alumium</p>
+              <h3 className="text-teal-600 m-0">Insights</h3> to your knowledge
+            </div>
           </div>
         </div>
       </div>
