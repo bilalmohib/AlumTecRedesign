362a44295e00ff4c20741062fd4793c3caf53d61
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2023-09-15 06:36:26 +0500

    Test (#14)
    * responsiveness of contact us page done
    
    * working on career page
    
    - Added Resume upload button and desktop version of careers page done
    
    * working on blogs page
    
    * alumtec website updated
    
    * fixed
    
    * removed next font
    
    * fixed and resolved errors

--- app/pageComponents/Home/HomeProjectsBlock/HPBHeader/index.tsx
+++ app/pageComponents/Home/HomeProjectsBlock/HPBHeader/index.tsx
@@ -1,12 +1,10 @@
-import { montserratFont } from "@/utils/fonts";
-
 import styles from "./style.module.css";
 
 const HPBHeader = () => {
   return (
     <>
       <h2
-        className={`${styles.heading} ${montserratFont} text-white text-6xl font-semibold leading-5 xl:leading-[77px] text-center`}
+        className={`${styles.heading} text-white text-6xl font-semibold leading-5 xl:leading-[77px] text-center`}
       >
         OUR PRODUCTS
       </h2>
