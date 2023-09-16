2a24c6fde7eba8b22ad0c39c731608c7027483e7
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2023-09-17 01:22:23 +0500

    Test (#21)
    * responsiveness of contact us page done
    
    * working on career page
    
    - Added Resume upload button and desktop version of careers page done
    
    * working on blogs page
    
    * alumtec website updated
    
    * fixed
    
    * removed next font
    
    * fixed and resolved errors
    
    * fixed error
    
    * working on alum tec design
    
    * work on blog description page
    
    * updated
    
    * removed borders
    
    * updated readme
    
    * fixed font issue
    
    * updated readme
    
    * readme updated
    
    * fixed career page issue
    
    * fixed footer

--- app/pageComponents/Blog/BlogBody/index.tsx
+++ app/pageComponents/Blog/BlogBody/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@mui/material";
 import BlogCard from "./BlogCard";
 import BlogHeader from "./BlogHeader";
 import { blogDataList } from "./data";
 import { BlogDataTypes } from "./types";
 import { useRouter } from "next/router";
+import Footer from "@/app/Components/Footer";
 
 const BlogBody = () => {
   const router = useRouter();
@@ -25,6 +27,19 @@ const BlogBody = () => {
           );
         })}
       </div>
+      <div className="mt-10 mb-8 w-full flex flex-col justify-center items-center">
+        <Button
+          variant="contained"
+          color="primary"
+          className="font-[Sharp Grotesk] text-lg mt-0 font-normal leading-[77px] rounded-sm bg-[#123E95] hover:bg-[#0b46bb] hover:text-white w-40 h-[50px]"
+          onClick={() => {
+            console.log("Button clicked");
+          }}
+        >
+          Load More
+        </Button>
+      </div>
+      <Footer />
     </div>
   );
 };
