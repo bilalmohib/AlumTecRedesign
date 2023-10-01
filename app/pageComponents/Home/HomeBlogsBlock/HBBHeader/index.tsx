90c9f0b576253f1099be0eb19287d6faae019b31
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2023-10-01 21:19:23 +0500

    Develop it now (#32)
    * responsiveness of contact us page done
    
    * feat
    
    * This is new branch created from develop (#30)
    
    * fixed

--- app/pageComponents/Home/HomeBlogsBlock/HBBHeader/index.tsx
+++ app/pageComponents/Home/HomeBlogsBlock/HBBHeader/index.tsx
@@ -1,14 +1,16 @@
-import styles from "./style.module.css";
-
 const HBBHeader = () => {
   return (
-    <div className={styles.headingContainer}>
-      <div className={styles.sliderOuterContainer}>
-        <div className={styles.box}>
-          <h2 className={styles.headingOverlay}>BLOGS</h2>
+    <div className="h-36 mt-10">
+      <div className="w-full h-auto relative">
+        <div className="w-full h-full absolute top-0 left-0 opacity-100 bg-[#0057e3]">
+          <h2 className="font-normal text-[99px] text-center leading-[77px] bg-gradient-to-r from-[rgba(0,0,0,0.02)] via-transparent to-[#ffffff] bg-cover bg-repeat bg-clip-text text-gray-100 select-none font-[Sharp Grotesk-Medium20, Helvetica]">
+            BLOGS
+          </h2>
         </div>
-        <div className={`${styles.box} ${styles.overlay}`}>
-          <h2 className={styles.heading}>OUR BLOGS</h2>
+        <div className="w-full h-full absolute top-0 left-0 opacity-100 bg-[#0057e3] z-10">
+          <h2 className="ml-[1.5rem] mt-[6.5rem] text-center text-[#000] pb-[1rem] font-bold font-[Sharp Grotesk-Medium20, Helvetica] text-6xl leading-[77px]">
+            OUR BLOGS
+          </h2>
         </div>
       </div>
     </div>
--- app/pageComponents/Home/HomeBlogsBlock/HomeBlogsBody/HomeBlogsList/index.tsx
+++ app/pageComponents/Home/HomeBlogsBlock/HomeBlogsBody/HomeBlogsList/index.tsx
@@ -8,63 +8,12 @@ interface HomeBlogsListProps {}
 const HomeBlogsList = (blogs: HomeBlogsListProps) => {
   return (
     <div className={styles.container}>
-      <div className={styles.leftContainer}>
-        <Image
-          className={styles.leftPatternsImage}
-          alt="Left Patterns Image"
-          src="/Images/Home/Blogs/Patterns.png"
-          width={112}
-          height={152}
-          loading="eager"
-        />
-        <Image
-          className={styles.rightPatternsImage}
-          alt="Right Patterns Image"
-          src="/Images/Home/Blogs/Patterns.png"
-          width={112}
-          height={152}
-          loading="eager"
-        />
-        <div className={styles.frameLeftBlogImage} />
-        <div className={styles.leftHBCard}>
-          <HBCard />
-        </div>
+      <div className="h-auto relative w-full">
+        <HBCard />
       </div>
-      <div className={styles.rightContainer}>
-        <div className={styles.frameRightBlogImage} />
-        <div className={styles.rightHBCard}>
-          <HBCard />
-        </div>
+      <div className="h-auto relative w-full ml-12">
+        <HBCard />
       </div>
-      {/* <div className={styles.rightContainer}>
-        <div className={styles.rectangle_4} />
-        <div className={styles.rectangle_5} />
-        <Image
-          className={styles.mask_group_2}
-          alt="Mask group"
-          title="Second Mask"
-          width={270}
-          height={400}
-          src="/Images/Home/Blogs/blog2.png"
-        />
-        <div className={styles.right_content_2}>
-          <p className={styles.the_startup_ultimate}>
-            The Startup Ultimate
-            <br />
-            Guide to Grow your
-            <br />
-            Business.
-          </p>
-          <div className={styles.text_wrapper_4}>by Admin</div>
-          <div className={styles.group_3}>
-            <div className={styles.text_wrapper_5}>12</div>
-            <div className={styles.text_wrapper_8}>July</div>
-          </div>
-          <div className={styles.button}>
-            <div className={styles.text_wrapper_7}>Learn More</div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
