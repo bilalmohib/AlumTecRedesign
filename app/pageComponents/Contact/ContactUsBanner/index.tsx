8d2bbbaa3d152095286a4220e5d403578b01ee7b
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2023-10-14 18:58:22 +0500

    update

--- app/pageComponents/Contact/ContactUsBanner/index.tsx
+++ app/pageComponents/Contact/ContactUsBanner/index.tsx
@@ -4,13 +4,13 @@ const ContactUsBanner = () => {
       <div className="relative h-60 sm:h-[80vh] w-full backdrop-filter backdrop-blur-0 ease-linear transition">
         <div className="absolute inset-0 bg-black opacity-10"></div>
         <h1 className="px-7 sm:px-8 md:px-6 absolute top-0 left-0 right-0 bottom-0 mt-8 sm:mt-[15vh] text-2xl sm:text-5xl font-semibold font-lato sm:font-sharp text-left sm:text-center text-white">
-          Let&rsquo;s help you navigate your next
+          Let&rsquo;s help you navigate your next project
         </h1>
-        <p className="hidden sm:block absolute top-0 left-0 right-0 bottom-0 mt-[28vh] w-8/12 text-lg text-center text-white ml-auto mr-auto">
+        {/* <p className="hidden sm:block absolute top-0 left-0 right-0 bottom-0 mt-[28vh] w-8/12 text-lg text-center text-white ml-auto mr-auto">
           In publishing and graphic design, Lorem ipsum is a placeholder text
           commonly used to demonstrate the visual form of a document or a
           typeface without relying
-        </p>
+        </p> */}
         <p className="px-7 sm:px-0 block sm:hidden absolute top-0 left-0 right-0 bottom-0 mt-[12vh] sm:mt-[28vh] w-full sm:w-8/12 text-xs sm:text-lg text-left sm:text-center text-white ml-auto mr-auto">
           AlumTec Glass and Aluminium offers a wide range of aluminium products
           and services. All our products comply with Dubai Green Building
