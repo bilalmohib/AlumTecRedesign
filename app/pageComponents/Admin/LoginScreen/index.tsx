9ca2a0b2248eca810b540f9c5f39913f3f5e2139
bilalmohib <bilalmohib7896@gmail.com>
2023-11-25 00:00:59 +0500

    fixed error

--- app/pageComponents/Admin/LoginScreen/index.tsx
+++ app/pageComponents/Admin/LoginScreen/index.tsx
@@ -47,7 +47,7 @@ const LoginScreen = () => {
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
+        const token = credential?.accessToken;
         // The signed-in user info.
         const user = result.user;
 
