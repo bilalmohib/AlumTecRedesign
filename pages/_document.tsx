9d3e446b5068cd4163d1383ebef2b64d9373482a
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2024-03-19 16:51:47 +0500

    Develop (#69)
    * responsiveness of contact us page done
    
    * feat
    
    * This is new branch created from develop (#30)
    
    * fixed
    
    * added phone numbers
    
    * fixed types errors
    
    * database integrated in contact form
    
    * update
    
    * update
    
    * removed blogs
    
    * feat
    
    * admin work
    
    * work on login
    
    * fixed error
    
    * working on admin
    
    * working on admin panel
    
    * working on adding services through admin panel
    
    - Local Setup done
    - Firebase database data save left
    
    * working on uploading blog section
    
    * working on admin blog upload functoinality
    
    * local blog functionality done
    
    * used firestore instead of firebase
    
    * updating success message
    
    * work
    
    * fixed
    
    * fix again
    
    * tailwind deployment error fixed
    
    * deployment issues fixed
    
    * fixed next config deployment error
    
    * fixed
    
    * feat
    
    * working on adding images in admin blog
    
    * working on blog page admin panel
    
    - working on image upload in blog
    - text content work done
    
    * fixed the blog admin work
    
    * working on admin panel
    
    * add blog submission to database functionality
    
    * working on blog. Blog added successully
    
    * wip of admin panel
    
    * code
    
    * worked on blog functionality
    
    * updated blog share whatsapp done
    
    * fetching blogs without login
    
    * removed extra logs
    
    * updated blogs
    
    * updated the blogs fetching using server side for effective SEO
    
    * updated firebase config
    
    * updated blogs infinite loop
    
    * added privacy policy
    
    * fixed the errors
    
    * fixed error
    
    * added login screen
    
    * Implemented Delete Blog Feature
    
    * deleted privact policy
    
    * removed blog filtering when viewing for admin
    
    * updated and added view section in admin blogs for viewing inquiries
    
    * removed the error in blogs
    
    * removed extra button
    
    * added sitemap
    
    * fixed error
    
    * fixed sitemap url
    
    * fixed
    
    * fixed error
    
    * updated fixed sitemap
    
    * Fixed responsiveness of most pages
    
    * Added home button
    
    * added seo information for better seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import organization from "@/app/data/organization.json";
 
 export default function Document() {
   return (
@@ -6,6 +7,7 @@ export default function Document() {
       <Head />
       <title>AlumTec</title>
       <link rel="icon" href="/logo.jxr" type="image/x-icon"></link>
+      <script type="application/ld+json">{JSON.stringify(organization)}</script>
       <body>
         <Main />
         <NextScript />
